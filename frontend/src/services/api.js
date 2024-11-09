00468c04e286481cd286e0c0898b920101011f48
ganesh9803 <santhosh.ganesh1023@gmail.com>
2024-11-10 02:55:12 +0530

    modif-9

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://mini-loan-application-backend.vercel.app/api',
 });
 
 // Request Interceptor to add token
