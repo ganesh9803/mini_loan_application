3d69cb82a6424e0ffa734df3ad0404e99ea57279
ganesh9803 <santhosh.ganesh1023@gmail.com>
2025-06-13 08:38:43 +0530

    new commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,8 +16,8 @@ const signup = async (req, res) => {
     }
 
     // Validate password length
-    if (password.length < 6) {
-      return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+    if (!password || password.lengt) {
+      return res.status(400).json({ message: "Password must be at least 6 characters long" });
     }
 
     // Hash the password
